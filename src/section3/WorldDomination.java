e8040a4854dd7c1044532e4bec8f6d3440085d86
root <root@5883d7b9ff27>
2020-08-12 21:57:24 +0000

    Day 3

--- src/section3/WorldDomination.java
+++ src/section3/WorldDomination.java
@@ -6,10 +6,16 @@ public class WorldDomination {
 	
 	public static void main(String[] args) {
 		// 1. Ask the user if they know how to write code.
-
+String input;
+input = JOptionPane.showInputDialog("Do you know how to write code?");
 		// 2. If they say "yes", tell them they will rule the world.
-
-		// 3. Otherwise, wish them good luck washing dishes.
+if (input.equalsIgnoreCase("yes")){
+	JOptionPane.showMessageDialog(null, "You rule the PS4 Supercoputer that the army uses.");
+	}
+else
+			JOptionPane.showMessageDialog(null,"Good luck washing dishes +  doing boring stuff.");
+	// 3. Otherwise, wish them good luck washing dishes.
+	
 
 	}
 }
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,17 +1,28 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+ int userscore = 0;
 		// 2.  Ask the user a question 
+ String answer1;
+ answer1 = JOptionPane.showInputDialog("2 + 2 =");
 		
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if(answer1.equals("4")) {
+			userscore = +1;
+		}	
+		else
+			userscore = -1;
+		JOptionPane.showMessageDialog(null, "You stupid it's 4.");
 		// 4.  if the user's answer was correct, add one to their score 
-		
+		String answer2;
+answer2 = JOptionPane.showInputDialog("Consider 10 days. if someone gave you 1 candy, and every day the candy(ies) clone themselves. how m"
+		+ "many candies would you have?");
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
 		
 		// 6.  After all the questions have been asked, print the user's score 
